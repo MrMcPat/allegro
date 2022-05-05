a715cfe9e439c9e46a2af5cbf37087cdab03c109
Patrick Liang <pl204511194@yahoo.com>
2022-05-05 13:39:21 -0400

    reworked time calculation

--- client/components/SetAlarm.js
+++ client/components/SetAlarm.js
@@ -42,95 +42,95 @@ export default function SetAlarm() {
     ) {
       Alert.alert("Start time must be greater than end time.");
     } else {
-      let hourDiff =
-        startTime.substring(3, 5) === endTime.substring(3, 5)
-          ? parseInt(startTime.substring(0, 2)) -
-            parseInt(endTime.substring(0, 2))
-          : parseInt(startTime.substring(0, 2)) -
-            parseInt(endTime.substring(0, 2)) -
-            1;
-      let minuteDiff;
-      hourDiff =
-        parseInt(startTime.substring(3, 5)) > parseInt(endTime.substring(3, 5))
-          ? hourDiff + 1
-          : hourDiff;
+      // let hourDiff =
+      //   startTime.substring(3, 5) === endTime.substring(3, 5)
+      //     ? parseInt(startTime.substring(0, 2)) -
+      //       parseInt(endTime.substring(0, 2))
+      //     : parseInt(startTime.substring(0, 2)) -
+      //       parseInt(endTime.substring(0, 2)) -
+      //       1;
+      // let minuteDiff;
+      // hourDiff =
+      //   parseInt(startTime.substring(3, 5)) > parseInt(endTime.substring(3, 5))
+      //     ? hourDiff + 1
+      //     : hourDiff;
 
-      if (
-        parseInt(startTime.substring(3, 5)) ===
-        parseInt(endTime.substring(3, 5))
-      ) {
-        minuteDiff = 0;
-      } else if (
-        parseInt(startTime.substring(3, 5)) > parseInt(endTime.substring(3, 5))
-      ) {
-        hourDiff + 1;
-        minuteDiff =
-          parseInt(startTime.substring(3, 5)) -
-          parseInt(endTime.substring(3, 5));
-      } else if (
-        parseInt(startTime.substring(3, 5)) < parseInt(endTime.substring(3, 5))
-      ) {
-        minuteDiff =
-          60 -
-          (parseInt(endTime.substring(3, 5)) -
-            parseInt(startTime.substring(3, 5)));
-      }
-      let totalTime = hourDiff * 60 + minuteDiff;
-      let totalDays = Math.floor(totalTime / increment) + 1;
-      const tomorrow = new Date();
-      tomorrow.setDate(tomorrow.getDate() + 1);
-      let dayArray = [JSON.stringify(tomorrow).substring(1, 11)];
-      let timeArray = [startTime];
-      for (let i = 2; i <= totalDays; i++) {
-        const nextDay = new Date();
-        nextDay.setDate(nextDay.getDate() + i);
-        dayArray.push(JSON.stringify(nextDay).substring(1, 11));
-        let hour;
-        let minute;
-        if (parseInt(startTime.substring(3, 5)) === 0) {
-          hour = parseInt(startTime.substring(0, 2)) - 1;
-          minute = 60 - increment * (i - 1);
-          for (
-            let j = 1;
-            j <
-            parseInt(startTime.substring(0, 2)) -
-              parseInt(endTime.substring(0, 2));
-            j++
-          ) {
-            if (minute < 0) {
-              hour--;
-              minute = 60 + minute;
-            }
-          }
-          timeArray.push(
-            `${hour.toString().padStart(2, 0)}:${minute
-              .toString()
-              .padStart(2, 0)}`
-          );
-        } else {
-          hour = parseInt(startTime.substring(0, 2));
-          minute = parseInt(startTime.substring(3, 5)) - increment * i;
-          for (
-            let j = 0;
-            j <=
-            parseInt(startTime.substring(0, 2)) -
-              parseInt(endTime.substring(0, 2));
-            j++
-          ) {
-            if (minute < 0) {
-              hour--;
-              minute = 60 + minute;
-            }
-          }
-          timeArray.push(
-            `${hour.toString().padStart(2, 0)}:${minute
-              .toString()
-              .padStart(2, 0)}`
-          );
-        }
-      }
-      console.log(JSON.stringify(dayArray));
-      console.log(JSON.stringify(timeArray));
+      // if (
+      //   parseInt(startTime.substring(3, 5)) ===
+      //   parseInt(endTime.substring(3, 5))
+      // ) {
+      //   minuteDiff = 0;
+      // } else if (
+      //   parseInt(startTime.substring(3, 5)) > parseInt(endTime.substring(3, 5))
+      // ) {
+      //   hourDiff + 1;
+      //   minuteDiff =
+      //     parseInt(startTime.substring(3, 5)) -
+      //     parseInt(endTime.substring(3, 5));
+      // } else if (
+      //   parseInt(startTime.substring(3, 5)) < parseInt(endTime.substring(3, 5))
+      // ) {
+      //   minuteDiff =
+      //     60 -
+      //     (parseInt(endTime.substring(3, 5)) -
+      //       parseInt(startTime.substring(3, 5)));
+      // }
+      // let totalTime = hourDiff * 60 + minuteDiff;
+      // let totalDays = Math.floor(totalTime / increment) + 1;
+      // const tomorrow = new Date();
+      // tomorrow.setDate(tomorrow.getDate() + 1);
+      // let dayArray = [JSON.stringify(tomorrow).substring(1, 11)];
+      // let timeArray = [startTime];
+      // for (let i = 2; i <= totalDays; i++) {
+      //   const nextDay = new Date();
+      //   nextDay.setDate(nextDay.getDate() + i);
+      //   dayArray.push(JSON.stringify(nextDay).substring(1, 11));
+      //   let hour;
+      //   let minute;
+      //   if (parseInt(startTime.substring(3, 5)) === 0) {
+      //     hour = parseInt(startTime.substring(0, 2)) - 1;
+      //     minute = 60 - increment * (i - 1);
+      //     for (
+      //       let j = 1;
+      //       j <
+      //       parseInt(startTime.substring(0, 2)) -
+      //         parseInt(endTime.substring(0, 2));
+      //       j++
+      //     ) {
+      //       if (minute < 0) {
+      //         hour--;
+      //         minute = 60 + minute;
+      //       }
+      //     }
+      //     timeArray.push(
+      //       `${hour.toString().padStart(2, 0)}:${minute
+      //         .toString()
+      //         .padStart(2, 0)}`
+      //     );
+      //   } else {
+      //     hour = parseInt(startTime.substring(0, 2));
+      //     minute = parseInt(startTime.substring(3, 5)) - increment * i;
+      //     for (
+      //       let j = 0;
+      //       j <=
+      //       parseInt(startTime.substring(0, 2)) -
+      //         parseInt(endTime.substring(0, 2));
+      //       j++
+      //     ) {
+      //       if (minute < 0) {
+      //         hour--;
+      //         minute = 60 + minute;
+      //       }
+      //     }
+      //     timeArray.push(
+      //       `${hour.toString().padStart(2, 0)}:${minute
+      //         .toString()
+      //         .padStart(2, 0)}`
+      //     );
+      //   }
+      // }
+      // console.log(JSON.stringify(dayArray));
+      // console.log(JSON.stringify(timeArray));
       if (!alarm) {
         axios.post("http://localhost:3000/alarms", {
           user_id: user.id,
